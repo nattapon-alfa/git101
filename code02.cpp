b4345365c3efc43d57a7799853ab52c3d10004ee
nattapon <nattapon.rag@ku.th>
2024-01-29 16:15:56 +0700

    hello

--- code02.cpp
+++ code02.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main() {
     // cout : console output
     // endl : end line '\n'
-    cout << "Hello Git" << endl;
+    cout << "love cpp" << endl;
 } // namespace std;
 
